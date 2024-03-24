934176e7bb0fea87f7a320a6d7699f80fbb728a9
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2024-03-24 12:00:18 -0400

    create views culture_list_view and culture_detail_view

--- apps/cultura/urls.py
+++ apps/cultura/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.cultura.views.cultura_views import cultura_view
+from apps.cultura.views.cultura_views import cultura_list_view,cultura_detail_view
 
 urlpatterns = [
-    path('cultura/',cultura_view,name='cultura_view'),
+    path('cultura/',cultura_list_view,name='cultura_list_view'),
+    path('cultura/<int:pk>', cultura_detail_view, name='cultura_detail_view'),
 ]
--- apps/cultura/views/cultura_views.py
+++ apps/cultura/views/cultura_views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from apps.cultura.models import CultureInstitution, CultureProject
 
-# vista para cultura
-def cultura_view(request):
-    return render(request,'cultura_view.html')
+
+# List all cultural institutions
+def cultura_list_view(request):
+    culture_institutions= CultureInstitution.objects.filter(active=True)
+    return render(request,'cultura_list_view.html',culture_institutions)
+
+
+# List the pk cultural institution with all its active projects
+def cultura_detail_view(request,pk):
+    culture_institution= CultureInstitution.objects.filter(
+        pk=pk,
+        active=True
+        ).first()
+    projects = CultureProject.objects.filter(
+        culture=pk,
+        active=True
+    )
+    context = {'culture':culture_institution, 'projects':projects}
+    return render(request,'cultura_detail_view.html',context)
