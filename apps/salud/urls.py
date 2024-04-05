10e549329d4dd9c21b6687582031924139bdf597
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2024-04-05 05:33:17 -0400

    create salud view

--- apps/salud/urls.py
+++ apps/salud/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from apps.salud.views.polyclinics_views import polyclinics_view,polyclinics_detail_view
 from apps.salud.views.pharmacy_views import pharmacy_view,pharmacy_detail_view
 from apps.salud.views.consultingRoom_views import consultingroom_view,consultingroom_detail_view
+from apps.salud.views.salud_view import salud_view
 
 
 urlpatterns = [
+    path('salud/',salud_view,name='salud_view'),
     path('polyclinics/',polyclinics_view,name='polyclinics_view'),
     path('polyclinics/<int:pk>',polyclinics_detail_view,name='polyclinics_detail_view'),
     path('pharmacy/',pharmacy_view,name='pharmacy_view'),
---
+++ apps/salud/views/salud_view.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+
+def salud_view(request):
+    return render(request,'salud_view.html')
